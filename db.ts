3d11b30386fda4d0e941382ec0344f8d4e662a6f
datnguyencypresshub <datnguyen.cypresshub@gmail.com>
2026-06-25 21:30:24 +0700

    feat/provide env for project

--- db.ts
+++ db.ts
@@ -1,13 +1,14 @@
 import mysql from 'mysql2/promise';
 
-// Khởi tạo Connection Pool giúp tối ưu kết nối, tránh tràn cổng kết nối
 export const pool = mysql.createPool({
-  host: '127.0.0.1',
-  user: 'root',
-  password: 'Nguyendat@123', // Viết mật khẩu gốc bình thường, KHÔNG cần mã hóa %40
-  database: 'wedding_db',
-  port: 3306,
+  host: process.env.MYSQL_HOST || '127.0.0.1',
+  user: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || 'Nguyendat@123', 
+  database: process.env.MYSQL_DATABASE || 'wedding_db',
+  port: Number(process.env.MYSQL_PORT) || 3306,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  // Thêm dòng này vì hầu hết các host MySQL online đều yêu cầu SSL mã hóa bảo mật
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: true } : undefined
 });
